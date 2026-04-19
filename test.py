b544cb74ec946366a63bf1ee27c8545813f51847
Prabhat Shastri <prabhatshastri@Prabhats-MacBook-Pro.local>
2026-04-19 17:08:52 -0400

    Made test.py

---
+++ test.py
@@ -0,0 +1,7 @@
+import pandas as pd
+import geopandas as gpd
+import matplotlib.pyplot as plt
+
+print("✓ Pandas version:", pd._version_)
+print("✓ GeoPandas version:", gpd._version_)
+print("✓ All packages working!")
