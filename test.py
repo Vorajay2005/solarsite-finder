def7fb0a1e928d914d1d0d5f148b56104fc094f5
Prabhat Shastri <prabhat.shastri.19@gmail.com>
2026-05-07 00:55:05 -0400

    New data load

--- test.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-import geopandas as gpd
-import matplotlib.pyplot as plt
-
-print("✓ Pandas version:", pd.__version__)
-print("✓ GeoPandas version:", gpd.__version__)
-print("✓ All packages working!")
